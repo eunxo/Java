ebb5d1c510f05db6ec01589883eb394ef17d10fe
eunxo <es2k1004@naver.com>
2025-09-23 12:26:15 +0900

    java03주차

---
+++ SMU/src/ch02/sec11/VariableScopeExample.java
@@ -0,0 +1,15 @@
+package ch02.sec11;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		int v1 = 15;
+		int v2 = v1 - 10;
+		if(v1>10) {
+			int v21 = v1 - 10;
+		}
+		int v3 = v1 + v2 +5;
+	}
+
+}

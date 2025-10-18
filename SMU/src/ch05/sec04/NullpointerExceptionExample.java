8656160efc6a4fc22d6e611339ed0cbc46219f94
eunxo_ <es2k1004@naver.com>
2025-10-18 23:28:30 +0900

    5주차과제

---
+++ SMU/src/ch05/sec04/NullpointerExceptionExample.java
@@ -0,0 +1,13 @@
+package ch05.sec04;
+
+public class NullpointerExceptionExample {
+
+	public static void main(String[] args) {
+		int[] intArray = null;
+		//intArray[0] = 10;
+		
+		String str = null;
+
+	}
+
+}
